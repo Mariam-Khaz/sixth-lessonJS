5cf650eb2287cba0fbf236fa965ce115d8f13ce9
Mariam-Khaz <mariam-khazroun@hotmail.com>
2024-11-23 14:23:59 +0100

    start-up

---
+++ script.js
@@ -0,0 +1,44 @@
+// Variables for the DOM elements
+
+// Array
+const words = [
+  "dependent",
+  "dog",
+  "superficial",
+  "admit",
+  "juice",
+  "javascript",
+  "developer",
+  "airplane",
+  "great",
+  "fun",
+  "manipulate",
+  "cat",
+  "transition",
+  "school",
+  "computer",
+  "programming",
+  "drag",
+  "loving",
+  "north",
+];
+
+
+if (words === "") {
+  return true
+} else {
+  return false
+}
+
+// console.dir(words);
+
+// document.getElementsByClassName(text)
+
+
+
+//============   PART 2   =========================
+
+setTimeout()
+function addWordToDOM(words) {
+  console.log()
+}
